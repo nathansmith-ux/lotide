a3b10a4f5a1437d6882c1ea3a1fafea79e9a7a9c
Nathan Smith <nathanrmsmith@gmail.com>
2023-07-12 15:44:14 -0700

    assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,20 @@
+// Function Implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("⛔⛔⛔ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// Comparing identical strings
+assertEqual("LHL", "LHL");
+
+// Comparing non-identical strings
+assertEqual("Lighthouse Labs", "Bootcamp");
+
+// Comparing identical numbers
+assertEqual(1, 1);
+
+// Comparing non-identical numbers
+assertEqual(1, 5);
