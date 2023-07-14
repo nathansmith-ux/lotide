2831777aa527b61441e5be02dc9b33e8b92a384a
Nathan Smith <nathanrmsmith@gmail.com>
2023-07-14 13:32:22 -0700

    Updated according to ESlint

--- eqArrays.js
+++ eqArrays.js
@@ -17,12 +17,12 @@ const eqArrays = function(array1, array2) {
     }
     return true;
   }
-}
+};
 
 // True Test Cases
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 
 // False Test Cases
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
