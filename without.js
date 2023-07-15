1ea40adab3147ac791ba3db22d3919f5cb065e3f
Nathan Smith <nathanrmsmith@gmail.com>
2023-07-15 10:13:30 -0700

    filtering and removing unwanted items

---
+++ without.js
@@ -0,0 +1,43 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ The source array was not altered`);
+  } else {
+    console.log(`⛔⛔⛔ The source array was altered`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  return source.filter(function(sourceItem) {
+    return !itemsToRemove.includes(sourceItem)
+  })
+}
+
+// String Test
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"]));
+
+// Numbers Test
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(without(numbers, [2, 4, 5]));
+
+// String & Numbers Test
+const stringAndNumbers = ["hello", 1, 3, "LHL", 18];
+console.log(without(stringAndNumbers, ["hello", 3]));
+
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(numbers, [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(stringAndNumbers, ["hello", 1, 3, "LHL", 18]);
