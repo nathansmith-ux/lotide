f9bf0795e2c1b1c6c6bde6969958b334443f749c
Nathan Smith <nathanrmsmith@gmail.com>
2023-07-18 18:00:08 -0700

    Finding keys by their value

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let keys = Object.keys(object)
+  for (let key of keys) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+  family: "Suite Life Of Zach & Cody",
+  suspence: "Succession",
+  action: "Equalizer",
+  horror: "Halloween",
+  romantic: "Notebook"
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Succession"), "suspence");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Notebook"), "romantic");
