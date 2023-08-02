b6a61c150fa15bdadd9432cd6a973465d236b91e
Nathan <nathanrmsmith@gmail.com>
2023-08-02 19:56:36 -0400

    eq as require added and function exported

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -19,13 +8,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-// True Test Case
-assertArraysEqual([5, 6, 7], [5, 6, 7]);
-assertArraysEqual(["5", "6", "7"], ["5", "6", "7"]);
-
-// False Test Case
-assertArraysEqual(["5", "6", "7"], ["5", "6", "8"]);
-assertArraysEqual(["5", "6", "7"], ["5", "6", 7]);
-assertArraysEqual([15, 6, 7], [5, 6, 7]);
-
-
+module.exports = assertArraysEqual;
