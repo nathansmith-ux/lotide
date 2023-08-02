82c0317c97a3d07f30dae82d98334d52a69be8fb
Nathan <nathanrmsmith@gmail.com>
2023-08-02 19:19:08 -0400

    tests for head and assertEqual added

---
+++ test/headTest.js
@@ -0,0 +1,14 @@
+const assertEqual = require('../assertEqual')
+const head = require('../head')
+
+// Returns Assertion Passed: 5 === 5
+assertEqual(head([5,6,7]), 5);
+
+// Returns Assertion Passed: Hello === Hello
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// Returns Assertion Passed: 6 === 6
+assertEqual(head([6]), 6);
+
+// Returns Assertion Passed: undefined === undefined
+assertEqual(head([]));
