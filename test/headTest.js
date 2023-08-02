3d4064ab92b1a2dab3c81ce876d01aa7f1879129
Nathan <nathanrmsmith@gmail.com>
2023-08-02 19:32:45 -0400

    eslint applied

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,5 @@
-const assertEqual = require('../assertEqual')
-const head = require('../head')
+const assertEqual = require('../assertEqual');
+const head = require('../head');
 
 // Returns Assertion Passed: 5 === 5
 assertEqual(head([5,6,7]), 5);
