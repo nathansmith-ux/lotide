3b5d1b791c18b643654608f5f2a3ebc712b2abef
Nathan <nathanrmsmith@gmail.com>
2023-08-02 19:47:59 -0400

    test for eqArrays added

---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+// True Test Cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+
+// False Test Cases
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
