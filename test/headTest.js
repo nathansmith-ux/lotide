4a2ba2a04ff79bba47ba0cc7b706ae15dc2909ec
Nathan <nathanrmsmith@gmail.com>
2023-08-03 10:54:12 -0400

    Mocha & chai test added

--- test/headTest.js
+++ test/headTest.js
@@ -1,14 +1,24 @@
-const assertEqual = require('../assertEqual');
 const head = require('../head');
+const assert = require('chai').assert;
 
-// Returns Assertion Passed: 5 === 5
-assertEqual(head([5,6,7]), 5);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1)
+  });
 
-// Returns Assertion Passed: Hello === Hello
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5')
+  });
 
-// Returns Assertion Passed: 6 === 6
-assertEqual(head([6]), 6);
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello')
+  });
 
-// Returns Assertion Passed: undefined === undefined
-assertEqual(head([]));
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined)
+  });
+
+  it("returns 6 for [6]", () => {
+    assert.strictEqual(head([6]), 6)
+  });
+});
