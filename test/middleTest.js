17a06a67e8948df857e0d054b948dc4517b780e7
Nathan <nathanrmsmith@gmail.com>
2023-08-02 20:10:08 -0400

    test created for middle.js function

---
+++ test/middleTest.js
@@ -0,0 +1,17 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+// Return Empty
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+
+// Array is an odd number total => Return A Single Element
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+
+// Array is an even number total => Return Multiple Elements
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+assertArraysEqual([], middle([1]));
+assertArraysEqual([2], middle([1, 2, 3]));
+assertArraysEqual([2, 3], middle([1, 2, 3, 4]));
