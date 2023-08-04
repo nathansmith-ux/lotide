f8092857b0193254f0b780daafb887e733194cf1
Nathan <nathanrmsmith@gmail.com>
2023-08-04 11:24:04 -0400

    fixed eslint issues

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,18 +3,18 @@ const assert = require('chai').assert;
 
 describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
 
   it("returns [2, 3, 4] when provided [1, 2, 3, 4]", () => {
-    assert.deepEqual(tail([1, 2, 3, 4]) [2, 3, 4])
-  })
+    assert.deepEqual(tail([1, 2, 3, 4]) [2, 3, 4]);
+  });
 
   it("returns an empty array when provided [1]", () => {
-    assert.deepEqual(tail([1]), [])
-  })
+    assert.deepEqual(tail([1]), []);
+  });
 
   it("returns an empty array when provided an empty array", () => {
-    assert.deepEqual(tail([]), [])
-  })
+    assert.deepEqual(tail([]), []);
+  });
 });
