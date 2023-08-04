7ac6b8774d2e7ddab1362f3b6b905501379a297c
Nathan <nathanrmsmith@gmail.com>
2023-08-04 13:07:51 -0400

    object with all the functions

---
+++ index.js
@@ -0,0 +1,27 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const without = require('./without');
+const flatten = require('./flatten');
+const middle = require('./middle');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const findKey = require('./findKey');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  without: without,
+  flatten: flatten,
+  middle: middle,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  letterPositions: letterPositions,
+  findKeyByValue: findKeyByValue,
+  map: map,
+  takeUntil: takeUntil,
+  findKey: findKey,
+};
