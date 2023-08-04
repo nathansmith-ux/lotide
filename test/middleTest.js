5c857da5e852a7f35c5261db0fb6d9ed3225c0cc
Nathan <nathanrmsmith@gmail.com>
2023-08-04 11:30:02 -0400

    mocha & chai test added with assert.deepEqual

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,18 +1,31 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-// Return Empty
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
+describe("#middle", () => {
+  it("returns an empty array when provided with an array with 1 element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-// Array is an odd number total => Return A Single Element
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
+  it("returns an empty array when provided with an array with 2 elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
 
-// Array is an even number total => Return Multiple Elements
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+  // Odd Number Tests
+  it("returns [2] when provided with [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
 
-assertArraysEqual([], middle([1]));
-assertArraysEqual([2], middle([1, 2, 3]));
-assertArraysEqual([2, 3], middle([1, 2, 3, 4]));
+  it("returns [3] when provided with [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  // Even Number Tests
+  it("returns [2, 3] when provided with [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [3, 4] when provided with [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
