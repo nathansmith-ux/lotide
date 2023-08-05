c067f4e53613be303742d2b193ecc288220c2973
Nathan <nathanrmsmith@gmail.com>
2023-08-05 15:20:22 -0400

    comments added using JS doc style and refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,20 @@
-// Function accepts two arrays and determines if they are equal
+/**
+ * Function determines if two arrays are equal
+ * @param {array} array1 
+ * @param {array} array2 
+ * @returns Boolean
+ */
 const eqArrays = function(array1, array2) {
-  // If elements do not have the same number of elements, return false.
   if (array1.length !== array2.length) {
     return false;
+  
   } else {
     for (let i = 0; i < array1.length; i++) {
-      // If the individual elements within the array are not equal, return false
       if (array1[i] !== array2[i]) {
         return false;
       }
     }
+    
     return true;
   }
 };
