ff8ba82914e77756363d0846358440734a03df1c
Nathan <nathanrmsmith@gmail.com>
2023-08-05 17:23:51 -0400

    Add missing functions

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Importing all the functions in the Lotide Library
+const assertEqual = require('./assertEqual');
 const head = require('./head');
 const tail = require('./tail');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual')
 const without = require('./without');
 const flatten = require('./flatten');
 const middle = require('./middle');
@@ -8,14 +11,19 @@ const countOnly = require('./countOnly');
 const countLetters = require('./countLetters');
 const letterPositions = require('./letterPositions');
 const findKeyByValue = require('./findKeyByValue');
+const eqObjects = require('./eqObjects');
+const assertObjectsEqual = require('./assertObjectsEqual');
 const map = require('./map');
 const takeUntil = require('./takeUntil');
 const findKey = require('./findKey');
 
 // Exporting all the functions in the Lotide Library as an object
 module.exports = {
+  assertEqual,
   head,
   tail,
+  eqArrays,
+  assertArraysEqual,
   without,
   flatten,
   middle,
@@ -23,6 +31,8 @@ module.exports = {
   countLetters,
   letterPositions,
   findKeyByValue,
+  eqObjects,
+  assertObjectsEqual,
   map,
   takeUntil,
   findKey,
