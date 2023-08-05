0929e702960b6b73b58d464d8cc1edb8606b806a
Nathan <nathanrmsmith@gmail.com>
2023-08-05 15:29:13 -0400

    comments added using JS doc style

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,12 @@
 // Importing eq Arrays function which checks if an array is equal
 const eqArrays = require('./eqArrays');
 
-// Function determines accepts two arrays and if they are equal returns a success message. Otherwise returns a fail message
+/**
+ * Functions determines if two arrays are equal
+ * @param {array} array1 
+ * @param {array} array2 
+ * @returns a success or failure message
+ */
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`✅✅✅ The provided arrays are a perfect match`);
