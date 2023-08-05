83c01dea274b54008404de5476e6f6ce40982f64
Nathan <nathanrmsmith@gmail.com>
2023-08-05 15:03:33 -0400

    added comments based on JS doc style to the function and refactored

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,19 +1,13 @@
-// Function compares if two primitive data types are strictly equal
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Function accepts an object and a key's value then returns the first key that matches the provided value
+/**
+ * Function returns the first key that matches the provided value
+ * @param {object} object 
+ * @param {string} value 
+ * @returns The key as a string
+ */
 const findKeyByValue = function(object, value) {
-  // Assigns all keys in the object to the keys variable
   let keys = Object.keys(object); 
-  // Loops through all the object keys
+
   for (let key of keys) { 
-    // Compares object's value with value passed through, if true it will return the key
     if (object[key] === value) { 
       return key;
     }
@@ -22,21 +16,3 @@ const findKeyByValue = function(object, value) {
 
 // Exporting Function
 module.exports = findKeyByValue;
-
-// Provided Object
-const bestTVShowsByGenre = {
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire",
-  family: "Suite Life Of Zach & Cody",
-  suspence: "Succession",
-  action: "Equalizer",
-  horror: "Halloween",
-  romantic: "Notebook"
-};
-
-// Testing To See If Function Works As Expected
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Succession"), "suspence");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Notebook"), "romantic");
