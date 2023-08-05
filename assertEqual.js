e69266c8aa4c95af8a062f98239fdf0f9df13948
Nathan <nathanrmsmith@gmail.com>
2023-08-05 14:54:45 -0400

    added comments based on JS doc style to the function

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,9 @@
-// Function will check if two primitive data types are equal
+/**
+ * Function will check if two primitive data types are equal
+ * @param {primitive data type} actual 
+ * @param {primitive data type} expected 
+ * @returns success or fail message
+ */
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
